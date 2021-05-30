da5dc298f25507cc1b0bc2a2df29020065a87f52
Alexandr Halypa <gwair1989@gmail.cm>
2021-05-30 12:30:29 +0300

    Final version

--- Water Timer/ViewController.swift
+++ Water Timer/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let systemSoundID: SystemSoundID = 1016
+    
+    
+    let dictionary = ["10 min": 10, "15 min": 15, "20 min": 20]
+    
+    var timer = Timer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        progressBar.progress = 0.0
     }
-
-
+    
+    @IBAction func pressedGlases(_ sender: UIButton) {
+        
+        guard let titelOfButtons = sender.currentTitle else {
+            return
+        }
+        
+        guard var getSeconds = dictionary[titelOfButtons] else {
+            return
+        }
+        
+        guard let staticSecond = dictionary[titelOfButtons] else {
+            return
+        }
+        
+        timer.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (infoLabel) in
+            if getSeconds > 0 {
+                self.infoLabel.text = "\(getSeconds) seconds"
+                getSeconds -= 1
+                let caunterForvard = staticSecond - getSeconds
+                print(caunterForvard)
+                self.progressBar.progress = Float(caunterForvard) / Float(staticSecond)
+            } else {
+                self.timer.invalidate()
+                self.infoLabel.text = "Done!\n\nYou nide to drink some wather!".uppercased()
+                AudioServicesPlaySystemSound(self.systemSoundID)
+            }
+        }
+    }
 }
-
